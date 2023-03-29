dbed2b8e975980ea853bfa434c7d54fffb6c74b8
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-03-29 15:23:52 +0300

    Little change to route protection

--- bulgarian-drivers/src/RouterOutlet.js
+++ bulgarian-drivers/src/RouterOutlet.js
@@ -1,8 +1,6 @@
-import { useContext } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import AuthContext from './contexts/authContext';
-
+import PageNotFound from './components/404page/PageNotFound';
 import CreatePost from './components/createPost/CreatePost';
 import DriversList from './components/drivers/DriversList';
 import HomePage from './components/home/HomePage';
@@ -10,38 +8,41 @@ import Login from './components/login/Login';
 import PostDetails from './components/postDetails/PostDetails';
 import Profile from './components/profile/Profile';
 import UserPosts from './components/profile/UserPosts';
+import ProtectedRoute from './components/ProtectedRoute';
 import Register from './components/register/Register';
 import Search from './components/search/Search';
-import ProtectedRoute from './components/UI/ProtectedRoute';
-import PageNotFound from './components/404page/PageNotFound';
 
 const RouterOutlet = () => {
-  const { user } = useContext(AuthContext);
-
   return (
     <Routes>
       <Route path="*" element={<PageNotFound />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/search" element={<Search />} />
-      <Route path="/create-post" element={
-        <ProtectedRoute user={user}>
-          <CreatePost />
-        </ProtectedRoute>
-      } />
       <Route path="/drivers" element={<DriversList />} />
       <Route path="/drivers/:id" element={<PostDetails />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/profile" element={
-        <ProtectedRoute user={user}>
-          <Profile />
-        </ProtectedRoute>
-      } />
-      <Route path="/user-posts" element={
-        <ProtectedRoute user={user}>
-          <UserPosts />
-        </ProtectedRoute>
-      } />
+      <Route path="/create-post"
+        element={
+          <ProtectedRoute>
+            <CreatePost />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/user-posts"
+        element={
+          <ProtectedRoute>
+            <UserPosts />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
--- bulgarian-drivers/src/components/ProtectedRoute.js
+++ bulgarian-drivers/src/components/ProtectedRoute.js
@@ -1,9 +1,15 @@
+import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 
+import AuthContext from '../contexts/authContext';
+
 const ProtectedRoute = (props) => {
-  if (!props.user) {
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
     return <Navigate to="/login" replace />;
   }
+  
   return props.children;
 };
 
