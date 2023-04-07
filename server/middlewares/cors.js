01c31767550b4206b8c32bd2421eae14b7e51906
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-04-07 14:20:15 +0300

    .

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -4,7 +4,7 @@ module.exports = () => (req, res, next) => {
     'http://localhost:3000',
   ];
 
-  if (corsWhiteList.indexOf(req.headers.origin !== -1)) {
+  if (corsWhiteList.indexOf(req.headers.origin) !== -1) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Authorization, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,');
